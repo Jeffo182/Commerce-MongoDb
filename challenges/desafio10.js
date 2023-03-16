f80c30b7ce539566f110e47486084117672bbae4
Jefferson <jefferson.felix182@gmail.com>
2023-03-16 01:46:06 -0300

    Desafio 11

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,8 @@
-db.produtos
-.find({
-    valoresNutricionais: { $elemMatch: { tipo: "proteínas", percentual: { $lte: 40, $gte: 30 } } },
-  }, { _id: 0, nome: 1 });
+db.produtos.find(
+  {
+    valoresNutricionais: {
+      $elemMatch: { tipo: "proteínas", percentual: { $lte: 40, $gte: 30 } },
+    },
+  },
+  { _id: 0, nome: 1 },
+);
